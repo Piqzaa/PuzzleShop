8eee136016f0dccbf07575db35b8c88ea92c7777
Alex B <alex.berrel@gmail.com>
2026-06-11 00:03:38 +0200

    init: plugin mypetpuzzle-core (bootstrap + classes vides)

---
+++ plugins/mypetpuzzle-core/includes/class-puzzle-product.php
@@ -0,0 +1,7 @@
+<?php
+
+defined('ABSPATH') || exit;
+
+class MyPetPuzzle_Puzzle_Product {
+
+}
---
+++ plugins/mypetpuzzle-core/mypetpuzzle-core.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: MyPetPuzzle Core
+ * Plugin URI:  https://mypetpuzzle.com
+ * Description: Fonctionnalités principales MyPetPuzzle.
+ * Version:     1.0.0
+ * Author:      Your Name
+ * Text Domain: mypetpuzzle-core
+ * Requires PHP: 8.2
+ * Requires Plugins: woocommerce
+ */
+
+defined('ABSPATH') || exit;
