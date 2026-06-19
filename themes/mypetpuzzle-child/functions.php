b2c1b74ae8838d74da357eebed02114d5e32919d
Alex B <alex.berrel@gmail.com>
2026-06-19 02:57:21 +0200

    chore: charger le nouveau fichier single-product.php

--- themes/mypetpuzzle-child/functions.php
+++ themes/mypetpuzzle-child/functions.php
@@ -5,3 +5,4 @@ defined('ABSPATH') || exit;
 require_once __DIR__ . '/functions/assets.php';
 require_once __DIR__ . '/functions/setup.php';
 require_once __DIR__ . '/functions/woocommerce.php';
+require_once __DIR__ . '/functions/single-product.php';
