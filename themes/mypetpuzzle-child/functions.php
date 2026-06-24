ab72a47355925fc74dc17a4814c52e2044245aa3
Alex B <alex.berrel@gmail.com>
2026-06-24 20:45:43 +0200

    fix: déplace la définition de MYPETPUZZLE_CUSTOM_PRODUCT_ID dans functions.php (disponible globalement)

--- themes/mypetpuzzle-child/functions.php
+++ themes/mypetpuzzle-child/functions.php
@@ -2,6 +2,8 @@
 
 defined('ABSPATH') || exit;
 
+defined('MYPETPUZZLE_CUSTOM_PRODUCT_ID') || define('MYPETPUZZLE_CUSTOM_PRODUCT_ID', 151);
+
 require_once __DIR__ . '/functions/assets.php';
 require_once __DIR__ . '/functions/setup.php';
 require_once __DIR__ . '/functions/woocommerce.php';
